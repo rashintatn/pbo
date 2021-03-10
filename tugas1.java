9d8a4067d147f157576fb39a043b32208e7762f3
rashintatn <80341330+rashintatn@users.noreply.github.com>
2021-03-10 18:38:08 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,23 @@
+public class tugas1 {
+    public static void main(String[] args) {
+        //SOAL 1
+        int soal1;
+        soal1 = -5 + 8 * 6;
+        System.out.println(soal1);
+        
+        //SOAL 2
+        int soal2;
+        soal2 = (55+9) % 9;
+        System.out.println(soal2);
+
+        //SOAL 3
+        int soal3;
+        soal3 = 20 + -3*5 / 8;
+        System.out.println(soal3);
+
+        //SOAL 4
+        int soal4;
+        soal4 = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(soal4);
+    }
+}
